660dcfa3bd7d27ba81a0db3bf949c2b5451dfb9a
Himanshu-ui-max <himanshusharma234567@gmail.com>
2024-10-24 22:17:55 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+
+const setToken=(userToken)=>{
+    localStorage.setItem('token', JSON.stringify(userToken));
+}
+
+const getToken=()=>{
+      const tokenstring = localStorage.getItem('token');
+      const userToken=JSON.parse(tokenstring);
+      return userToken?.token
+    }
 
 function App() {
+   const [token, settoken] = useState(getToken());
+  if(!token){
+    return <Login settoken={settoken}/> 
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>TaskRabbit</h1>
+    
+    <Routes>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+    </>
   );
 }
 
